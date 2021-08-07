0127ff2e4211b44755fc1c59f8afbb770f7af317
jason <jasonabisaad@hotmail.com>
2021-08-07 15:58:48 +0300

    comment changed index.php

--- public/index.php
+++ public/index.php
@@ -5,5 +5,5 @@ foreach($_POST as $key=>$value){
     $postParameters[$key]=$value;  //put POST args in the array if they exist
 }
 session_start();//start a session to use its variable
-$app = new App($postParameters);//initiate App class which will route to the controller and funciton provided in the url
+$app = new App($postParameters);//initiate App class which will route to the controller and funciton provided in the URL
 ?>
